2ae301f2528b40db1f83101fb5946664d4badcf8
Lily O'Sullivan <lilyosullivan@pm.me>
2022-06-10 17:09:52 +0100

    Improved backtracking choice-points

--- Prolog/main.pl
+++ Prolog/main.pl
@@ -4,26 +4,12 @@
 % :- ['setup'].
 
 
-function(ReturnType,FunctionName,Params,Body) :-
+function(_,_,Params,Body) :-
     ptc_solver__clean_up,
     ptc_solver__default_declarations,
 
-    statement_handler(Params),
-    statement_handler(Body),
-    
-    ptc_solver__label_integers(Params).
+    param_handler(Params),
+    statement_handler(Body,Params,_).
 
 
-% params_to_list([],Acc,Out).
-%     Out = Acc.
-
-% params_to_listt([],Acc,Out).
-%     Out = Acc.
-
-% params_to_list([int(H)|T],Acc,Out) :-
-%     append([H],Acc,Out),
-%     params_to_listt(T,Acc,Out).
-
-% params_to_listt([int(H)|T],Acc,Out) :-
-%     append([H],Out,Acc),
-%     params_to_list(T,Acc,Out).
+ 
--- Prolog/statement_handler.pl
+++ Prolog/statement_handler.pl
@@ -1,35 +1,46 @@
 
-statement_handler([]).
-
-statement_handler([H|T]) :-
+param_handler([]).
+param_handler([H|T]) :-
     handle(H),
-    statement_handler(T).
+    param_handler(T).
+
+statement_handler([],_,_).
+statement_handler([H|T],Params,ReturnFlag) :-
+    handle(H,Params,ReturnFlag),
+    (
+        ReturnFlag == true
+    )
+        ;
+    (
+        statement_handler(T,Params,ReturnFlag)
+    ).
 
 handle(int(X)) :-
     ptc_solver__variable([X],integer).
 
-handle(ifStmt(Constraint,If_body)) :-
-    handle(ifStmt(Constraint,If_body,[])).
+handle(ifStmt(Constraint,If_body),Params,ReturnFlag) :-
+    ptc_solver__sdl(Constraint),
+    statement_handler(If_body,Params,ReturnFlag).
 
-% handle(ifStmt(Constraint,If_body)) :-
-%     (
-%         ptc_solver__sdl(Constraint)
-%             ->
-%                 statement_handler(If_body)
-%     ).
 
-handle(ifStmt(Constraint,If_body,Else_body)) :-
+    % handle(ifStmt(Constraint,If_body,[]),ReturnFlag).
+
+
+handle(ifStmt(Constraint,If_body,Else_body),Params,ReturnFlag) :-
     (
-        ptc_solver__sdl(Constraint)
-            ->
-                statement_handler(If_body)
-            ;
-                statement_handler(Else_body)
-
+        ptc_solver__sdl(Constraint),
+        statement_handler(If_body,_,ReturnFlag)
+    )
+        ;
+    (
+        ptc_solver__sdl(not Constraint),
+        statement_handler(Else_body,Params,ReturnFlag)
     ).
 
-handle(return(Expression)) :-
-    writeln(Expression).
+handle(return(Expression),Params,ReturnFlag) :-
+    writeln(Expression),
+    ptc_solver__label_integers(Params),
+    ReturnFlag = true.
 
 
 
