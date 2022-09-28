95197d1005154799c3cfd73e8030be396c5708c9
Lily O'Sullivan <lilyosullivan@pm.me>
2022-09-28 19:21:48 +0100

    Added error checks in Overcast generated module

--- overcast/template.c
+++ overcast/template.c
@@ -5,7 +5,7 @@
 typedef {{.resultType}} (*{{.functionNameType}})({{ .argumentTypes}});
 
 extern "C"
-int p_external_call_{{.functionName}}()
+int p_external_call()
 {
     EC_word return_value = EC_arg(1);
     {{ range .declarations }}{{ stringConverter . }}
@@ -16,10 +16,20 @@ int p_external_call_{{.functionName}}()
 
     std::string path = "{{.pathToDll}}" + std::string("\\") + "{{.dllNameWithExtension}}";
     HINSTANCE dll = LoadLibrary(path.c_str());
+    if (dll == NULL)
+    {
+        unify(return_value, EC_atom("Error loading dll"));
+        return EC_fail;
+    }
+
     {{.functionNameType}} {{.functionName}} = ({{.functionNameType}}) GetProcAddress(dll,"{{.functionName}}");
+    if ({{.functionName}} == NULL)
+    {
+        unify(return_value, EC_atom("Error loading function"));
+        return EC_fail;
+    }
 
     {{.resultType}} result = {{.functionName}}({{ joinDeclarationVarNames .declarations}});
-
     unify(return_value, EC_word(result));
     return EC_succeed;
 }
