8d5762cf846256f48f93c71825cee03eb23bd8a0
Lily O'Sullivan <lilyosullivan@pm.me>
2023-01-12 20:54:11 +0000

    Missed utils file on previous push

--- Prolog/utils.pl
+++ Prolog/utils.pl
@@ -1,24 +1,4 @@
-:- module(utils).
-
-:- export utils__var_name/2.
-:- export utils__get_all_array_inputs/2.
-:- export utils__evaluate_to_int/2.
-:- export utils__ptc_type/2.
-:- dynamic utils__var_name/2.
-:- dynamic var_names/1.
-
 :- lib(ptc_solver).
-
-%HACK: The function below was greatly hacked. It needs to be fixed up.
-%% Reads from the .names file
-utils__var_name(Prolog_var_name, C_var_name) :-
-    retract(names_file(Names_filename))@main,
-    !,
-    compile(Names_filename),
-    asserta(names_file(Names_filename))@main,
-    !,
-    var_names(Prolog_var_name, C_var_name),
-    !.
 
 % From Eileen's Code
 utils__get_all_array_inputs([], []).
