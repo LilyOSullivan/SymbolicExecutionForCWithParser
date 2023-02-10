9a43705ffa016f06b53ddc74482a5e54ae06f63d
Lily O'Sullivan <lilyosullivan@pm.me>
2023-02-10 10:26:58 +0000

    Simplified loading of parsed prolog code

--- Prolog/main.pl
+++ Prolog/main.pl
@@ -104,14 +104,17 @@ main(Filename_without_extension, Function_name,Path_to_C_file) :-
     % https://www.ascii-code.com
 
     concat_string([Path_to_C_file, "/", Filename_without_extension, ".pl"], Prolog_file),
-    open(Prolog_file, read, Stream),
-    repeat,
-       read(Stream, Term),
-       (  Term == end_of_file->
-            !, close(Stream)
-       ;
-            assertz(Term)
-       ),
+    [Prolog_file],
+    
+    % open(Prolog_file, read, Stream),
+    % repeat,
+    %    read(Stream, Term),
+    %    (  Term == end_of_file->
+    %         !, close(Stream)
+    %    ;
+    %         assertz(Term)
+    %    ),
+
     % compile(Prolog_file,[output:print,verbose:1,opt_level:0,debug:on]),
     function_definition(Function_name, Params, Body, Return_type), % Match from compiled prolog file
     !,
