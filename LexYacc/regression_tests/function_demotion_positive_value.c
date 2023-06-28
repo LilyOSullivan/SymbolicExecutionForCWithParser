d2e36babdd5bc95e538d9a3b6ce4037d07f3d069
Lily O'Sullivan <lilyosullivan@pm.me>
2023-06-28 10:49:00 +0300

    Fixed function name in regression test

--- LexYacc/regression_tests/function_demotion_positive_value.c
+++ LexYacc/regression_tests/function_demotion_positive_value.c
@@ -1,6 +1,6 @@
 
 // Tests the demotion of a positive value.
 
-char function_demotion_negative_value() {
-    return 123456; // This becomes (+)64
+char function_demotion_positive_value() {
+    return 123456; // This becomes (+)64, '@' in ASCII
 }
